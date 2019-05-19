fa87eb38f02aa64d52e47376a3fc418ab943ae99
Johan <joh.pik.fr@gmail.com>
2019-05-19 11:49:02 +1000

    Header render Simple Navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return(
-    <div>Header</div>
+    <div className="header">
+      <h3>Just A Dash beauty</h3>
+      <ul className="header-list">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/catalogue">Catalogue</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">contact</Link>
+        </li>
+      </ul>
+    </div>
   )
 }
 
