8831ba53e652f223a1871b98d7b223e45538d198
Johan <joh.pik.fr@gmail.com>
2019-05-22 21:36:22 +1000

    Style the header a bit more user friendly

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,69 +6,35 @@ const Header = () => {
     <div className="header">
       <h3>Just A Dash beauty</h3>
 
-      <ul className="header-list">
-
-        <li className="main-list-element">
+        <div class="navbar">
           <Link to="/">Home</Link>
-        </li>
 
+          <div class="dropdown">
+            <button class="dropbtn">Skin Type</button>
+              <div class="dropdown-content">
+                <Link to="/catalogue/skin-type/oily">Oily</Link>
+                <Link to="/catalogue/skin-type/dry">Dry</Link>
+                <Link to="/catalogue/skin-type/sensitive">Sensitive</Link>
+                <Link to="/catalogue/skin-type/problematic">Problematic</Link>
+                <Link to="/catalogue/skin-type/normal">Normal</Link>
+              </div>
+          </div>
 
-        <li className="main-list-element">
-          Skin Type
-          <ul className="sub-list">
-            <li>
-              <Link to="/catalogue/skin-type/oily">Oily</Link>
-            </li>
-            <li>
-              <Link to="/catalogue/skin-type/dry">Dry</Link>
-            </li>
-            <li>
-              <Link to="/catalogue/skin-type/sensitive">Sensitive</Link>
-            </li>
-            <li>
-              <Link to="/catalogue/skin-type/problematic">Problematic</Link>
-            </li>
-            <li>
-              <Link to="/catalogue/skin-type/normal">Normal</Link>
-            </li>
+          <div class="dropdown">
+            <button class="dropbtn">Product Type</button>
+              <div class="dropdown-content">
+                <Link to="/catalogue/product-type/cleanser">cleanser</Link>
+                <Link to="/catalogue/product-type/toningMist">toningMist</Link>
+                <Link to="/catalogue/product-type/exfoliant">exfoliant</Link>
+                <Link to="/catalogue/product-type/serum">serum</Link>
+                <Link to="/catalogue/product-type/moisturiser">moisturiser</Link>
+                <Link to="/catalogue/product-type/treatment">treatment</Link>
+              </div>
+          </div>
 
-          </ul>
-        </li>
-
-
-        <li className="main-list-element">
-          Product Type
-          <ul className="prod-list">
-            <li>
-              <Link to="/catalogue/product-type/cleanser">cleanser</Link>
-            </li>
-            <li>
-              <Link to="/catalogue/product-type/toningMist">toningMist</Link>
-            </li>
-            <li>
-              <Link to="/catalogue/product-type/exfoliant">exfoliant</Link>
-            </li>
-            <li>
-              <Link to="/catalogue/product-type/serum">serum</Link>
-            </li>
-            <li>
-              <Link to="/catalogue/product-type/moisturiser">moisturiser</Link>
-            </li>
-            <li>
-              <Link to="/catalogue/product-type/treatment">treatment</Link>
-            </li>
-          </ul>
-        </li>
-
-        <li className="main-list-element">
           <Link to="/about">About</Link>
-        </li>
-
-        <li className="main-list-element">
           <Link to="/contact">contact</Link>
-        </li>
-
-      </ul>
+          </div>
 
     </div>
   )
