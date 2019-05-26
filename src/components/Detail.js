fe98c0cc9ad84c87e5cb22f4c11ec2b0c9f95c52
Johan <joh.pik.fr@gmail.com>
2019-05-26 16:54:53 +1000

    Details Page only renders if product match URL

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,7 +1,22 @@
-import React from "react"
+import React, { useState } from 'react'
+import { currentProductSelected, prodNames } from '../ressources/ProductList'
 
-const Detail = () => {
-  return <h2>DETAILS</h2>
+import NoMatch from './NoMatch'
+
+const Detail = (props) => {
+
+  // the page id Either a skin type or a product type ex: oily, dry, cleanser
+  const pageId = props.match.params.id
+
+  return ( (prodNames.includes(pageId)) ? (
+      <div>
+        <h2>DETAILS</h2>
+        <p>{pageId}</p>
+      </div>
+  ) : (
+    <NoMatch />
+  )
+    )
 }
 
 export default Detail
