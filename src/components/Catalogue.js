e18dd0918ae601bb2189cb5802bd0c6af01e796f
Johan <joh.pik.fr@gmail.com>
2019-05-26 21:10:45 +1000

    Detail Page works as expected

--- src/components/Catalogue.js
+++ src/components/Catalogue.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-import { productList, skinTypes, prodTypes, currentProductSelected } from '../ressources/ProductList'
+import { productList, skinTypes, prodTypes } from '../ressources/ProductList'
 
 import NoMatch from './NoMatch'
 
@@ -10,24 +10,23 @@ const Catalogue = (props) => {
   // const [prodList, setProdList] = useState(productList);
   // The STATE MOTHER FUCKER
   const [prodList] = useState(productList);
-  const [prodSelected, setProdSelected] = useState(currentProductSelected);
 
   // the page id Either a skin type or a product type ex: oily, dry, cleanser
   const pageId = props.match.params.id
   const pageType = props.match.params.type
 
-  const singleItem = ({ id, name, subName, description, img  }) => (
-    <div key={id} className="product-container">
-      <h3>{name}</h3>
-      <h4>{subName}</h4>
-      <Link to="/">
-      <img src={img} alt={name} className="image-thumbnail"/>
+  const singleItem = (prod) => (
+    <div key={prod.id} className="product-container">
+      <h3>{prod.name}</h3>
+      <h4>{prod.subName}</h4>
+      <Link to={`/catalogue/product-detail/${prod.url}`}>
+      <img src={prod.img} alt={prod.name} className="image-thumbnail"/>
       </Link>
     </div>
   )
 
 
-  // Find the right product to display on the page
+  // Find the right list of product to display on the page
   const renderList = () => {
     if (pageType === "skin-type") {
       return prodList.filter( prod => prod.skinType.includes(pageId)).map(prod => singleItem(prod) )
@@ -42,9 +41,9 @@ const Catalogue = (props) => {
       <h2>{pageId} product</h2>
       <div className="product-list">{renderList()}</div>
     </div>
-  ) : (
+    ):(
     <NoMatch />
-  )
+    )
   )
 }
 
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,18 +1,59 @@
-import React, { useState } from 'react'
-import { currentProductSelected, prodNames } from '../ressources/ProductList'
+import React, { Fragment, useState, useEffect } from 'react'
 
+import { Link } from 'react-router-dom'
+
+import { productList, prodNames } from '../ressources/ProductList'
 import NoMatch from './NoMatch'
 
 const Detail = (props) => {
-
-  // the page id Either a skin type or a product type ex: oily, dry, cleanser
+  // the id of the page contained in the url ex: goodByeSunshine
   const pageId = props.match.params.id
 
+  // The State Mother Fucker
+  const [ currentProduct, setCurrentProduct ] = useState(null)
+  // const [ inCart, setInCart ] = useState(false)
+
+  useEffect(
+    () => {
+      const product = productList.filter(
+        prod => prod.url.includes(pageId)
+      )
+      setCurrentProduct(product[0])
+      // console.log(currentProduct);
+    }, [currentProduct, pageId] // Do not Understand why it does this
+  )
+
+  const renderPage = () => {
+
+    const {
+      // id,
+      name,
+      // url,
+      subName,
+      skinType,
+      productType,
+      description
+      // loveList,
+      // directions,
+      // ingedients,
+      // size,
+      // price,
+      // img,
+      // inCart
+    } = currentProduct
+
+    return (
+      <Fragment>
+        <button onClick={() => window.history.back()}>go back</button>
+        <h2>{name} <span>{subName}</span></h2>
+        <h3>{skinType}/{productType}</h3>
+        <p>{description}</p>
+      </Fragment>
+    )
+  }
+
   return ( (prodNames.includes(pageId)) ? (
-      <div>
-        <h2>DETAILS</h2>
-        <p>{pageId}</p>
-      </div>
+      !currentProduct ? <p>Loading</p> : <div>{renderPage()}</div>
   ) : (
     <NoMatch />
   )
