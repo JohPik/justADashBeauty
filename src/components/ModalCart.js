e7b5cdd8520e210456b9d6b626f441b82f6e633f
Johan <joh.pik.fr@gmail.com>
2019-06-07 21:08:44 +1000

    Start to render Modal

--- src/components/ModalCart.js
+++ src/components/ModalCart.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react'
 import { ProductConsumer } from './context'
+import { Link } from 'react-router-dom'
 
 export default class Modal extends Component {
   render() {
     return (
-      <div>
-        hello from modal
-      </div>
+      <ProductConsumer>
+        {(value) => {
+          console.log(value);
+          const { cart } = value
+          const product = cart[cart.length - 1]
+          if (cart.length > 0) {
+            return (
+              <div>
+                <h3>Product has been added to the cart</h3>
+                <p>{product.name}</p>
+                <button>keep Shopping</button>
+                <Link to="/cart"><button>Cart</button></Link>
+              </div>
+            )
+          } else {
+
+            return null
+          }
+        }
+        }
+      </ProductConsumer>
     )
   }
 }
