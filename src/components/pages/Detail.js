a283cfbb5147ea100907474e0c8597b26258eb88
Johan <joh.pik.fr@gmail.com>
2019-06-16 16:47:00 +1000

    Changed Detail from funtional to Class Component

--- src/components/pages/Detail.js
+++ src/components/pages/Detail.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState, useEffect } from 'react'
+import React, { Fragment, Component } from 'react'
 import { ProductConsumer } from '../context'
 
 import { prodNames } from '../../ressources/ProductList'
@@ -6,63 +6,66 @@ import { prodNames } from '../../ressources/ProductList'
 import NoMatch from './NoMatch'
 import ModalCart from '../ModalCart'
 
-const Detail = (props) => {
+class Detail extends Component {
 
-  const [qty, setQty] = useState(1) //Page State
-  // the id of the page contained in the url ex: goodByeSunshine
-  const pageId = props.match.params.id
+  state = { qty: 1 }
 
-  const { addToCart, modalOpen, openModal, closeModal } = props.value
-  const checkModal = () => !modalOpen ? null : closeModal()
+  pageId = this.props.match.params.id
 
-  useEffect(() => {
-    checkModal();
-  }, []);
+  checkModal = () => this.props.value.modalOpen ? this.props.value.closeModal() : null;
 
-
-
-  // The actual content component rendering
-  const renderProduct = () => {
-    let currentProduct = props.value.productList.filter( prod => prod.url.includes(pageId))
-    let { id, name, subName, skinType, productType, description, inCart, price} = currentProduct[0]
-
-    return (
-      <Fragment>
-
-        <button onClick={ () => window.history.back() }>go back</button>
-
-        <h2>{name} <span>{subName}</span></h2>
-        <h3>{skinType}/{productType}</h3>
-        <h3>${price}</h3>
-        <p>{description}</p>
-
-        <div className="button-section">
-          { inCart ? null :
-            <div className="prod-qty-section">
-              { qty }
-              <button onClick={() => setQty(qty + 1)}>+</button>
-              <button onClick={() => qty === 1 ? null : setQty(qty - 1)}>-</button>
-            </div>
-          }
-          <button className="add-to-cart" disabled={inCart} onClick={ () => { addToCart(id, qty); openModal() } }>
-            { inCart ? <p>in cart</p> : <p>Add to Cart</p> }
-          </button>
-        </div>
-      {modalOpen ? <ModalCart /> : null}
-      </Fragment>
-    )
+  componentWillUnmount(){
+    this.checkModal()
   }
 
 
-  return ( (prodNames.includes(pageId)) ? (
-        renderProduct()
-    ) : (
-      <NoMatch />
-    )
-  )
-}
+  render(){
 
-// export default Detail
+    const { addToCart, modalOpen } = this.props.value
+
+    const openModal = () => this.props.value.openModal()
+
+    const renderProduct = () => {
+      let props = this.props
+      let qty = this.state.qty
+      let currentProduct = props.value.productList.filter( prod => prod.url.includes(this.pageId))
+      let { id, name, subName, skinType, productType, description, inCart, price} = currentProduct[0]
+
+      return (
+        <Fragment>
+          <h2 className="lol">{this.modalOpen}</h2>
+          <button onClick={ () => window.history.back() }>go back</button>
+
+          <h2>{name} <span>{subName}</span></h2>
+          <h3>{skinType}/{productType}</h3>
+          <h3>${price}</h3>
+          <p>{description}</p>
+
+          <div className="button-section">
+            { inCart ? null :
+              <div className="prod-qty-section">
+                { qty }
+                  <button onClick={() => this.setState({ qty: qty + 1 }) }>+</button>
+                <button onClick={() => this.state.qty === 1 ? null : this.setState({ qty: this.state.qty - 1 }) }>-</button>
+              </div>
+            }
+            <button className="add-to-cart" disabled={inCart} onClick={ () => { addToCart(id, qty); openModal() } }>
+              { inCart ? <p>in cart</p> : <p>Add to Cart</p> }
+            </button>
+          </div>
+        {modalOpen ? <ModalCart /> : null}
+        </Fragment>
+      )
+    }
+
+    return ( (prodNames.includes(this.pageId)) ? (
+          renderProduct()
+      ) : (
+        <NoMatch />
+      )
+    )
+  }
+}
 
 
 export default React.forwardRef((props, ref) => (
