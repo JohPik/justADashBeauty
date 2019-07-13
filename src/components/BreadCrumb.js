12f429782d85b5c1cf3e732e8c8e100560132bf7
Johan <joh.pik.fr@gmail.com>
2019-07-13 11:15:08 +1000

    Styling BreadCrumb

--- src/components/BreadCrumb.js
+++ src/components/BreadCrumb.js
@@ -5,35 +5,36 @@ const BreadCrumb = (props) => {
 
   const renderMe = () => {
     const { type, id, prodId } = props.match.params
-    // console.log("BreadCrumb Props", props)
+    const path  = props.match.path.substring(1)
+    console.log("BreadCrumb Props", props)
 
     if (type && id) { //Product Category
       return(
-        <div className="breadcrumb-container">
-           <Link to="/">Home</Link> > {type}: {id}
-        </div>
+        <section className="breadcrumb-container">
+           <Link to="/">Home</Link> > <span>{type}: {id}</span>
+        </section>
       )
     } else if (prodId) { // Single Product just loaded
       const prodHistory = props.location.state
       const prodName = props.prodName
       if (prodHistory) {
         return (
-          <div className="breadcrumb-container">
-             <Link to="/">Home</Link> > <Link to={`/catalogue/${prodHistory.type}/${prodHistory.id}`}>{prodHistory.type}: {prodHistory.id}</Link> > {prodName}
-          </div>
+          <section className="breadcrumb-container">
+             <Link to="/">Home</Link> > <Link to={`/catalogue/${prodHistory.type}/${prodHistory.id}`}>{prodHistory.type}: {prodHistory.id}</Link> > <span>{prodName}</span>
+          </section>
         )
       } else {
         return (
-          <div className="breadcrumb-container">
-             <Link to="/">Home</Link> > {prodName}
-          </div>
+          <section className="breadcrumb-container">
+             <Link to="/">Home</Link> > <span>{prodName}</span>
+          </section>
         )
       }
     } else { //Other Page
       return(
-        <div className="breadcrumb-container">
-           <Link to="/">Home</Link>
-        </div>
+        <section className="breadcrumb-container">
+           <Link to="/">Home</Link> > <span>{path}</span>
+        </section>
       )
     }
   }
