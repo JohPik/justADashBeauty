9b749114faab3d4e3ca9fdeb12c62607a8b4f322
Johan <joh.pik.fr@gmail.com>
2019-07-27 21:20:59 +1000

    Using List in Catalogue instead with filters

---
+++ src/components/pages/CatalogueList.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react'
+import { ProductConsumer } from '../context'
+
+// import SideBarFilter from './SideBarFilter'
+import ThumbnailProd from './ThumbnailProd'
+
+
+class CatalogueList extends Component {
+
+  state = {
+    nbrOfProd: 18,
+    skinType: "all",
+    prodType: "all"
+  }
+
+  /* HANDLE SIDE BAR SERACH*/
+  handleChangeSkinType = (event) =>  {
+    this.setState({skinType: event.target.value});
+  }
+
+  handleChangeProdType = (event) =>  {
+    this.setState({prodType: event.target.value});
+  }
+
+  handleSubmit = (event) =>  {
+    alert("hello")
+    event.preventDefault();
+  }
+
+
+  /* List Rendering*/
+  renderList = (value) => {
+    if (this.state.skinType === "all" && this.state.prodType === "all" ) {
+      return value.productList.map(prod => <ThumbnailProd prod={prod} key={prod.id}/>)
+    } else if (this.state.prodType === "all") {
+      return value.productList.filter( prod => prod.skinType.includes(this.state.skinType)).map(prod => <ThumbnailProd prod={prod} key={prod.id}/> )
+    } else if (this.state.skinType === "all") {
+      return value.productList.filter( prod => prod.productType.includes(this.state.prodType)).map(prod => <ThumbnailProd prod={prod} key={prod.id}/> )
+    } else {
+      const nbrOfProd = value.productList.filter( prod => prod.skinType.includes(this.state.skinType) && prod.productType.includes(this.state.prodType) )
+      return nbrOfProd.length === 0 ? <p>Sorry no product match you research</p> : nbrOfProd.map(prod => <ThumbnailProd prod={prod} key={prod.id}/>)
+    }
+  }
+
+  render(){
+    console.log(this.state);
+    return (
+      <section className="shop">
+        <h1 className="underline">Shop</h1>
+          <section className="sidebar-filter">
+               <form onSubmit={this.handleSubmit}>
+                <select name="skinType" value={this.state.skinType} onChange={this.handleChangeSkinType}>
+                  <option value="all">All</option>
+                  <option value="oily" defaultValue >Oily</option>
+                  <option value="problematic">Problematic</option>
+                  <option value="sensitive">Sensitive</option>
+                  <option value="normal">Normal</option>
+                  <option value="dry">Dry</option>
+                </select>
+
+                <select name="prodType" value={this.state.prodType} onChange={this.handleChangeProdType}>
+                  <option value="all">All</option>
+                  <option value="cleanser">Cleanser</option>
+                  <option value="toningMist">Toning Mist</option>
+                  <option value="exfoliant">Exfoliant</option>
+                  <option value="serum">Serum</option>
+                  <option value="moisturiser">Moisturiser</option>
+                  <option value="treatment">Treatment</option>
+                </select>
+
+                <input type="submit" value="Filter" />
+              </form>
+          </section>
+        <hr/>
+        <section className="shop-list">
+          {this.renderList(this.props.value)}
+        </section>
+      </section>
+    )
+  }
+}
+
+
+export default React.forwardRef((props, ref) => (
+  <ProductConsumer>
+    {value => <CatalogueList {...props} value={value} ref={ref} />}
+  </ProductConsumer>
+));
