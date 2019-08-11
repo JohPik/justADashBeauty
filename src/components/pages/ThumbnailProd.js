286f61c215fb5cbe5f06658b1b65d593aac57eea
Johan <joh.pik.fr@gmail.com>
2019-08-11 15:38:57 +1000

    Created "Already InCart" Span in ShopList

--- src/components/pages/ThumbnailProd.js
+++ src/components/pages/ThumbnailProd.js
@@ -6,23 +6,22 @@ import { Link } from 'react-router-dom'
 class ThumbnailProd extends Component {
 
   render(){
-    const { id, name, subName, url, img } = this.props.prod
+    const { id, name, subName, url, img, inCart } = this.props.prod
     return (
       <div key={id} className="product-container">
-        <Link to={{
-            pathname: `/shop/product-detail/${url}`
-          }}>
-          <div class="img-container">
+        <Link to={{ pathname:`/shop/product-detail/${url}` }}>
+          <div className="img-container">
             <img src={img} alt={name} className="image-thumbnail"/>
           </div>
         </Link>
-        
+
         <Link to={{
             pathname: `/shop/product-detail/${url}`
           }}>
         <h3>{name}</h3>
         </Link>
         <h4>{subName}</h4>
+        { inCart ? <span>Alredy in cart</span> : null }
       </div>
     )
   }
