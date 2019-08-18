e927a3ae4602e48b2cc0603576b5265615c54bdf
Johan <joh.pik.fr@gmail.com>
2019-08-19 07:01:53 +1000

    Toglle beteent Show Filters Button and Filters Form in CurrentFilter

--- src/components/pages/CatalogueList.js
+++ src/components/pages/CatalogueList.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import { ProductConsumer } from '../context'
 
 // import SideBarFilter from './SideBarFilter'
@@ -43,14 +43,14 @@ class CatalogueList extends Component {
   /* HANDLE SIDE BAR SERACH */
   handleChangeSkinType = (event) =>  {
     const temp = {...this.state.temp}
-    temp.skinTypeTemp = event.target.value;
+    temp.skinTypeTemp = event.target.value
     this.setState({ temp })
   }
 
   handleChangeProdType = (event) =>  {
 
     const temp = {...this.state.temp}
-    temp.prodTypeTemp = event.target.value;
+    temp.prodTypeTemp = event.target.value
     this.setState({ temp })
   }
 
@@ -61,6 +61,13 @@ class CatalogueList extends Component {
     event.preventDefault();
     this.props.history.push(`/shop/skintype=${skinType}&prodtype=${prodType}`)
   }
+
+  // resetFilter = (type) => {
+  //   const temp = {...this.state.temp}
+  //   const skinType = "all"
+  //   temp.skinTypeTemp = "all"
+  //   this.setState({ skinType,temp })
+  // }
 
 
   /* List Rendering */
@@ -81,41 +88,18 @@ class CatalogueList extends Component {
     return (
       <section className="shop">
         <h1 className="underline">Shop</h1>
-          <section className="current-filter">
-          <CurrentFilter skinType={this.state.skinType} prodType={this.state.prodType} />
-          </section>
 
-          <section className="sidebar-filter">
-               <form onSubmit={this.handleSubmit}>
-
-                 <div className="form-select">
-                  <label>Select a Skin Type:</label>
-                  <select name="skinType" value={this.state.temp.skinTypeTemp} onChange={this.handleChangeSkinType}>
-                    <option value="all">All</option>
-                    <option value="oily" defaultValue >Oily</option>
-                    <option value="problematic">Problematic</option>
-                    <option value="sensitive">Sensitive</option>
-                    <option value="normal">Normal</option>
-                    <option value="dry">Dry</option>
-                  </select>
-                  </div>
-
-                <div className="form-select">
-                  <label>Select a Product Type:</label>
-                  <select name="prodType" value={this.state.temp.prodTypeTemp} onChange={this.handleChangeProdType}>
-                    <option value="all">All</option>
-                    <option value="cleanser">Cleanser</option>
-                    <option value="toningMist">Toning Mist</option>
-                    <option value="exfoliant">Exfoliant</option>
-                    <option value="serum">Serum</option>
-                    <option value="moisturiser">Moisturiser</option>
-                    <option value="treatment">Treatment</option>
-                  </select>
-                </div>
-
-                <input type="submit" value="Filter" />
-              </form>
-          </section>
+        <section className="filter-container">
+          <CurrentFilter
+            skinType={this.state.skinType}
+            prodType={this.state.prodType}
+            temp={this.state.temp}
+            handleChangeSkinType={this.handleChangeSkinType}
+            handleChangeProdType={this.handleChangeProdType}
+            handleSubmit={this.handleSubmit}
+            resetFilter={this.resetFilter}
+            />
+        </section>
         <hr/>
         <section className="shop-list">
           {this.renderList(this.props.value)}
--- src/components/pages/CurrentFilter.js
+++ src/components/pages/CurrentFilter.js
@@ -3,7 +3,11 @@ import React, { Component, Fragment } from 'react'
 
 export default class CurrentFilter extends Component {
 
-filter = () => {
+  state = {
+    showForm: false
+  }
+
+showCurrentFilter = () => {
   const { skinType, prodType } = this.props
 
   if (skinType === "all" && prodType === "all") {
@@ -13,8 +17,9 @@ filter = () => {
       return (
         <Fragment>
           <h2>Your Current Filter</h2>
-          <div className="filter">
-            <span>Skin Type | {skinType}</span>
+          <div className="single-filter">
+            Skin Type <span>| {skinType}</span>
+          {/*<button onClick={() => this.props.resetFilter(skinType)}>x</button>*/}
           </div>
         </Fragment>
       )
@@ -22,11 +27,11 @@ filter = () => {
       return(
         <Fragment>
           <h2>Your Current Filters</h2>
-          <div className="filter">
-            <span>Skin Type | {skinType}</span>
+            <div className="single-filter">
+            Skin Type <span>| {skinType}</span>
           </div>
-          <div className="filter">
-            <span>Product Type | {prodType}</span>
+          <div className="single-filter">
+            Product Type <span>| {prodType}</span>
           </div>
         </Fragment>
         )
@@ -35,20 +40,66 @@ filter = () => {
     return (
       <Fragment>
         <h2>Your Current Filter</h2>
-        <div className="filter">
-          <span>Product Type | {prodType}</span>
+          <div className="single-filter">
+          Product Type <span>| {prodType}</span>
         </div>
       </Fragment>
     )
   }
+
+
 }
 
 render() {
   console.log("props", this.props);
     return (
       <Fragment>
-        {this.filter()}
-      </Fragment>
+
+        {this.state.showForm ? null :
+          <div className="current-filters">
+            {this.showCurrentFilter()}
+            <div className="show-form-button-container">
+              <button className="show-form" onClick={ () => this.setState({ showForm: true })}>Show Filters</button>
+            </div>
+          </div>
+        }
+
+        {this.state.showForm ? (
+          <div className="sidebar-filter" style={ this.state.showForm ? null :{ display: 'none'} }>
+               <form onSubmit={this.props.handleSubmit}>
+
+                 <div className="form-select">
+                  <label>Select a Skin Type:</label>
+                  <select name="skinType" value={this.props.temp.skinTypeTemp} onChange={this.props.handleChangeSkinType}>
+                    <option value="all">All</option>
+                    <option value="oily" defaultValue >Oily</option>
+                    <option value="problematic">Problematic</option>
+                    <option value="sensitive">Sensitive</option>
+                    <option value="normal">Normal</option>
+                    <option value="dry">Dry</option>
+                  </select>
+                  </div>
+
+                <div className="form-select">
+                  <label>Select a Product Type:</label>
+                  <select name="prodType" value={this.props.temp.prodTypeTemp} onChange={this.props.handleChangeProdType}>
+                    <option value="all">All</option>
+                    <option value="cleanser">Cleanser</option>
+                    <option value="toningMist">Toning Mist</option>
+                    <option value="exfoliant">Exfoliant</option>
+                    <option value="serum">Serum</option>
+                    <option value="moisturiser">Moisturiser</option>
+                    <option value="treatment">Treatment</option>
+                  </select>
+                </div>
+
+                <input type="submit" value="Filter" onClick={ () =>  this.setState({showForm: false}) }/>
+              </form>
+          </div>
+        ) : null}
+
+
+    </Fragment>
     )
   }
 }
