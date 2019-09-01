04c48a13c6d693554ac311a6e92b8da2c71ab228
Johan <joh.pik.fr@gmail.com>
2019-09-01 19:30:46 +1000

    Made Great progress on Styling Cart Component

--- src/components/pages/Cart.js
+++ src/components/pages/Cart.js
@@ -9,45 +9,38 @@ const Cart = () => {
     return (
       <div className="current-cart-container">
         <h2>Your Cart</h2>
-        { cart.length < 0 ? (
-          <p>Cart is empty</p>
+        { cart.length < 1 ? (
+          <p className="empty-cart">Your Cart is empty time to do some <Link to="shop/skintype=all&prodtype=all">Shopping</Link></p>
           ) : (
-            <Fragment>
-            <p className="underline">Number of product in the cart is equal to: {cart.length}</p>
-            <hr/>
-            <section className="cart-content">
-              {cart.map(product => {
-                return(
-                  <div key={product.id} className="cart-content-single">
+          <div className="cart-content">
+            {cart.map(product => {
+              return(
+                <div key={product.id} className="cart-content-single">
 
-                    <div className="img-container">
-                      <img src={product.img} alt={product.name} className="image-thumbnail"/>
-                    </div>
+                  <div className="img-container">
+                    <img src={product.img} alt={product.name} className="image-thumbnail"/>
+                  </div>
 
-                    <div className="prod-info-container">
-                      <h3>{product.name}</h3>
-                      <h4>{product.subName}</h4>
-                    </div>
-
-                     <p>A${product.price} <span>(Unit Price)</span></p>
-
+                  <div className="prod-info-container">
+                    <h3>{product.name}</h3>
+                    <h4>{product.subName}</h4>
                       <div className="prod-qty-container">
                         <p>Quantity</p>
                         <button className="qty-slct">+</button>
                           {product.count}
                         <button className="qty-slct">-</button>
                        </div>
-
-                    <p>Total Product Price: ${product.total}</p>
-
-                    <span>DELETE</span>
-
                   </div>
-                   )}
-                )}
-           </section>
 
-            </Fragment>
+                  <div className="align-right">
+                    <p>AU$ {product.total}</p>
+                    <button>DELETE</button>
+                  </div>
+
+                </div>
+                 )}
+              )}
+         </div>
         )}
       </div>
     )
@@ -60,11 +53,11 @@ const Cart = () => {
         <p className="summary-subtotal">Subtotal <span>A$ {total}</span></p>
         { total < 100 ? (
           <Fragment>
-            <p className="summary-shipping">ShippingCosts <span>A$ 20.00</span></p>
-            <span>Spend A$ { 100 - total } more and get free shipping</span>
+            <p className="summary-shipping">Shipping Costs <span>A$ 20.00</span></p>
+            <span className="spend-more-shipping">Spend A$ { 100 - total } more and get free shipping</span>
           </Fragment>
         ) : (
-          <p className="summary-shipping">ShippingCosts <span>A$ 0.00</span></p>
+          <p className="summary-shipping">Shipping Costs <span>A$ 0.00</span></p>
         )}
         <p className="summary-total">Total <span>A$ {total}</span></p>
         <button className="paypal-button">Checkout with Paypal</button>
@@ -76,9 +69,11 @@ const Cart = () => {
     return (
       <div className="help-container">
         <h2>Help</h2>
-        <Link to="/">Shipping and Delivery</Link>
-        <Link to="/">Payment and Returns</Link>
-        <Link to="/">Contact Us</Link>
+        <div className="help-links">
+          <Link to="/">Shipping and Delivery</Link>
+          <Link to="/">Payment and Returns</Link>
+          <Link to="/">Contact Us</Link>
+        </div>
       </div>
     )
   }
