0d33af80e57f1506805c4f5e62fb01679b8ed035
Johan <joh.pik.fr@gmail.com>
2020-02-22 15:33:30 +1000

    Quick edit of About PAGE

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,21 +7,21 @@ const About = (props) => {
     return (
     <Fragment>
       <BreadCrumb match={props.match}/>
-        <section className="about-page">
+        <section classNameName="about-page">
 
-            <h1 className="underline">About</h1>
+            <h1 classNameName="underline">About</h1>
             <p>Hi there,<br/><br/>
               I’m so happy you stopped by to read more about us.
               <br/><br/>
-              I’m Greta and I am the founder of <span class="bold">JUST A DASH.</span> I'm originally from Italy and I moved permanently to sunny Queensland about 5 years ago. For the past few years I have pretty much just worked in hospitality while continuing to pursue my hobby (or obsession, let’s face it) of making cosmetics at home. I started to make my own personal care products many years ago, out of frustration.
+              I’m Greta and I am the founder of <span className="bold">JUST A DASH.</span> I'm originally from Italy and I moved permanently to sunny Queensland about 5 years ago. For the past few years I have pretty much just worked in hospitality while continuing to pursue my hobby (or obsession, let’s face it) of making cosmetics at home. I started to make my own personal care products many years ago, out of frustration.
               <br/>I tried so many different products over the past few years, but I have never found a brand I truly loved. There was always a problem: false marketing claims, nasty ingredients, ridiculous price, off-putting smell... and the list could go on and on!
               <br/><br/>
               So one day, just like that, I quit my job and enrolled in a diploma of personal care formulation in order to become a fully qualified cosmetic chemist.
               <br/><br/>
-              I decided to take the situation into my own hands and start an affordable skincare line especially created for young women. After many months in the making, <span class="bold">JUST A DASH</span> is finally completed  and I am really proud to have put on the market some incredible and affordable products that look and smell amazing and deliver great results, only using the best performing and proven natural ingredients!<br/><br/>
+              I decided to take the situation into my own hands and start an affordable skincare line especially created for young women. After many months in the making, <span className="bold">JUST A DASH</span> is finally completed  and I am really proud to have put on the market some incredible and affordable products that look and smell amazing and deliver great results, only using the best performing and proven natural ingredients!<br/><br/>
               I would love to see young women embracing their own skin and not afraid to leave the house without heavy make-up on.
               <br/><br/></p>
-              <p>Here’s what we, at <span class="bold">JUST A DASH</span>, stand for:</p>
+              <p>Here’s what we, at <span className="bold">JUST A DASH</span>, stand for:</p>
               <ul>
                 <li>Only the best, natural and effective ingredients will ever go on your face and body</li>
                 <li>No furry friends have been or will ever be hurt in the making of our products</li>
@@ -29,8 +29,8 @@ const About = (props) => {
               </ul>
               We are always happy to create a custom skincare routine to match your skin type and help you smash those #skingoals so feel free to get in touch on messenger/instagram or by shooting us an email!
               <br/><br/>
-              <p class="italic">-Greta</p>
-              <p class="bold hot-pink">JUST A DASH</p>
+              <p className="selima">-Greta</p>
+              <p className="bold">JUST A DASH</p>
         </section>
     </Fragment>
     )
