68ef785969e0586e0ca1606c643c6ada8b0df1e4
Johan <joh.pik.fr@gmail.com>
2020-02-26 18:23:26 +1000

    Edit 404 Page

--- src/components/pages/NoMatch.js
+++ src/components/pages/NoMatch.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import { Link } from 'react-router-dom';
 
 const NoMatch = (props) => {
-  return(
-    <div>Error 404</div>
+  return (
+    <Fragment>
+      <section className="404-page">
+        <h1 className="underline">Error 404</h1>
+        <p>Ooops Wrong page!</p>
+        <button className="smart">
+          <Link to="/">RETURN TO THE HOME PAGE</Link>
+        </button>
+      </section>
+    </Fragment>
   )
 }
 
